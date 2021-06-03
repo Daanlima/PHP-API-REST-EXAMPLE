70e9fd09e70ddb5e927fe3714f19e261f120944d
Daniel LF <hss1_xp@hotmail.com>
2021-06-02 21:57:19 -0300

    Update listar.php

--- view/Usuario/listar.php
+++ view/Usuario/listar.php
@@ -4,9 +4,10 @@ $usuario = new UsuarioControl();
 
 header('Content-Type: application/json');
 
-foreach($usuario->findAll() as $valor){
+/*foreach($usuario->findAll() as $valor){
 	echo json_encode($valor);
-}
+}*/
 
+json_decode('{idCarro":"1","PlacaCarro":"ABC-1234","MarcaCarro":"Fiat","ModeloCarro":"Ford Ka","AnoCarro":"2018","Quilometragem":"14000","usuario_id":"1}');
 
 ?>
