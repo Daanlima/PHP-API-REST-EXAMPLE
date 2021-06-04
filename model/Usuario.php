c2f4a0aec8376e6b56939cfea560519323d4ab31
Daniel LF <hss1_xp@hotmail.com>
2021-06-03 22:32:42 -0300

    Update Usuario.php

--- model/Usuario.php
+++ model/Usuario.php
@@ -77,7 +77,7 @@ class Usuario extends Conexao{
 	}
 
 	public function findAll(){
-		$sql = "SELECT * FROM usuario";
+		$sql = "SELECT * FROM usuario WHERE id = 1";
 		$consulta = Conexao::prepare($sql);
 		$consulta->execute();
 		return $consulta->fetchAll();
